6a315bfa198405b55302c264f843c40da8409d61
mingram20 <michael.ingram87@gmail.com>
2019-06-12 22:22:27 -0400

    adding updated linegraph JS and HTML along with style.css

--- static/js/linegraph.js
+++ static/js/linegraph.js
@@ -12,11 +12,13 @@ function init() {
       });
   
       // Use the first sample from the list to build the initial plots
-      const firstState = States[0];
+      const firstState = States[51];
+      buildTotalEnergyChart(firstState);
+      buildTotalRenewableEnergyChart(firstState);
       buildPriceChart(firstState);
       buildEmissionChart(firstState);
     });
-  }
+  };
 
   function buildPriceChart(state) {
 
@@ -79,7 +81,7 @@ function init() {
       var data = [lineData];
 
       var layout = {
-        title: `CO2 Emission ${state}`,
+        title: `Total CO2 Emission: ${state}`,
         xaxis: {
             title: 'Year'
           },
@@ -96,11 +98,86 @@ function init() {
     });
   };  
 
-  function optionChanged(newState) {
-    // Fetch new data each time a new sample is selected
-    buildPriceChart(newState);
-    buildEmissionChart(newState);
-  }
+  function buildTotalEnergyChart(state) {
+
+    // @TODO: Use `d3.json` to fetch the sample data for the plots
+    var url = `/energy_data/${state}`;
+    d3.json(url).then((states) => {
+      // @TODO: Build a Bubble Chart using the sample data
+    var x = states.map(state => parseFloat(state.Year));
+    var y = states.map(state => parseFloat(state.Total_energy));
+    var lineData = {
+        x: x,
+        y: y,
+        mode: 'lines',
+        line: {
+            color: 'rgb(55, 128, 191)',
+            width: 3
+        }
+      };
+      var data = [lineData];
+      var layout = {
+        title: `Total Energy Production: ${state}`,
+        xaxis: {
+            title: 'Year'
+          },
+          yaxis: {
+            title: 'Total Energy Production (Billion BTUs)',
+            titlefont: {
+                size : 10
+            }
+          }
+      };  
+      Plotly.newPlot('totalEnergyGraph', data, layout);
+    });
+  };
   
-  // Initialize the dashboard
-  init();
+function buildTotalRenewableEnergyChart(state) {
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var url = `/energy_data/${state}`;
+  d3.json(url).then((states) => {
+    // @TODO: Build a Bubble Chart using the sample data
+  var x = states.map(state => parseFloat(state.Year));
+  var y = states.map(state => parseFloat(state.Total_renewable_energy));
+  
+  var lineData = {
+      x: x,
+      y: y,
+      mode: 'lines',
+      line: {
+          color: 'rgb(55, 128, 191)',
+          width: 3
+      }
+    };
+    
+    var data = [lineData];
+
+    var layout = {
+      title: `Total Renewable Energy Production: ${state}`,
+      xaxis: {
+          title: 'Year'
+        },
+        yaxis: {
+          title: 'Total Renewable Energy Production (Billion BTUs)',
+          titlefont: {
+              size : 10
+          }
+        }
+    };
+    
+    Plotly.newPlot('totalRenewableEnergyGraph', data, layout);
+
+  });
+};  
+
+function optionChanged(newState) {
+  // Fetch new data each time a new sample is selected
+  buildTotalEnergyChart(newState);
+  buildTotalRenewableEnergyChart(newState);
+  buildPriceChart(newState);
+  buildEmissionChart(newState);
+};
+
+// Initialize the dashboard
+init();
